05bdf88020adf65be42b4b766c534e0be0803e19
Tamas Levai <levait@tmit.bme.hu>
2018-12-10 17:21:21 +0100

    Add Shuffle
    The Shuffle module shuffles the content of incoming batches on basis
    of the Durstenfeld (Fisher-Yates) algorithm.

---
+++ our_plugins/modules/shuffle.cc
@@ -0,0 +1,58 @@
+// Copyright (c) 2014-2017, The Regents of the University of California.
+// Copyright (c) 2016-2017, Nefeli Networks, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// * Neither the names of the copyright holders nor the names of their
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "shuffle.h"
+
+CommandResponse Shuffle::Init(const bess::pb::ShuffleArg &) {
+  return CommandSuccess();
+}
+
+void Shuffle::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
+  size_t b_size = batch->cnt();
+  bess::PacketBatch *buf = &buf_;
+  buf->Copy(batch);
+  bess::Packet** pkts = buf->pkts();
+  bess::Packet** orig_pkts = batch->pkts();
+
+  for(size_t i=0; i < b_size; i++) {
+    size_t j = rng_.GetRange(i+1);
+    if (i != j) {
+      pkts[i] = pkts[j];
+    }
+    pkts[j] = orig_pkts[i];
+  }
+
+  bess::PacketBatch *new_batch = ctx->task->AllocPacketBatch();
+  new_batch->Copy(buf);
+  buf->clear();
+  RunNextModule(ctx, new_batch);
+}
+
+ADD_MODULE(Shuffle, "shuffle", "shuffles packets in batches")
